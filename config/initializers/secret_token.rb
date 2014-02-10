0d2643abe0599e276c3b4e1aa44a8510476cb2c1
flowkater <chjw102@gmail.com>
2014-02-11 06:14:11 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CerberusBack::Application.config.secret_key_base = '7b3178baf38dc3525c268a835f7a87e999bbc13ea6b0e48dd1c7fa3574b246aabe31f4cfecc0fdd801e77edf82e51a2eca4726eed87804aa1eedee838b3650e0'
