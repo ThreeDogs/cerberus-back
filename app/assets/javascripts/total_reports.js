4377062187252cc2613ecd40b5bcbd2a7aeec02d
kangho99 <KHY@Kenster-MacBook-Air.local>
2014-03-27 14:05:33 +0900

    temporary icon images embedded

--- app/assets/javascripts/total_reports.js
+++ app/assets/javascripts/total_reports.js
@@ -108,7 +108,117 @@ function drawPies() {
 
 }
 
-function drawTestFailure () {
+function drawTestFailPies() {
+
+	var fail_data = {
+			"A":[20,80],
+			"B":[20,80],
+			"C":[20,80],
+			"D":[20,80]
+			};
+
+	var test_fail_svg = d3.select("#test_failure")
+						.append("svg")
+						.attr("width",1150)
+						.attr("height",250);
+
+	var arc = d3.svg.arc()
+			.outerRadius(90)
+			.innerRadius(43);
+
+	var innerarc = d3.svg.arc()
+				.outerRadius(53)
+				.innerRadius(43);
+
+	var pie = d3.layout.pie()
+				.sort(null)
+				.value(function (d) {return d});
+
+	var pieA = test_fail_svg.selectAll(".arc A")
+				.data(pie(fail_data.A))
+				.enter()
+				.append("g")
+				.attr("transform", "translate(150,115)")
+				.attr("class","arc");
+	pieA.append("path")
+		.attr("d",arc)
+		.style("fill",function (d, i) {if (i==1) return "#C1633E"; else return "#EA7C4B";});
+	pieA.append("path")
+		.attr("d",innerarc)
+		.style("fill",function (d, i) {if (i==1) return "#A35338"; else return "#CE6C46";});
+	pieA.append("text")
+		.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+		.attr("dy","0.3em")
+		.attr("fill","white").attr("font-size","25px")
+		.style("text-anchor", "middle")
+		.text(function (d, i) {if (i==1) return "80"; else return "20";});
+
+	var pieB = test_fail_svg.selectAll(".arc B")
+				.data(pie(fail_data.B))
+				.enter()
+				.append("g")
+				.attr("transform", "translate(420,115)")
+				.attr("class","arc");
+	pieB.append("path")
+		.attr("d",arc)
+		.style("fill",function (d, i) {if (i==1) return "#BF7593"; else return "#ED9FBD";});
+	pieB.append("path")
+		.attr("d",innerarc)
+		.style("fill",function (d, i) {if (i==1) return "#8E516D"; else return "#CE91AA";});
+	pieB.append("text")
+		.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+		.attr("dy","0.3em")
+		.attr("fill","white").attr("font-size","25px")
+		.style("text-anchor", "middle")
+		.text(function (d, i) {if (i==1) return "80"; else return "20";});
+
+	var pieC = test_fail_svg.selectAll(".arc C")
+				.data(pie(fail_data.C))
+				.enter()
+				.append("g")
+				.attr("transform", "translate(690,115)")
+				.attr("class","arc");
+	pieC.append("path")
+		.attr("d",arc)
+		.style("fill",function (d, i) {if (i==1) return "#34989A"; else return "#52C4D0";});
+	pieC.append("path")
+		.attr("d",innerarc)
+		.style("fill",function (d, i) {if (i==1) return "#2D7C7C"; else return "#3AA4A7";});
+	pieC.append("text")
+		.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+		.attr("dy","0.3em")
+		.attr("fill","white").attr("font-size","25px")
+		.style("text-anchor", "middle")
+		.text(function (d, i) {if (i==1) return "80"; else return "20";});
+
+	var pieD = test_fail_svg.selectAll(".arc D")
+				.data(pie(fail_data.D))
+				.enter()
+				.append("g")
+				.attr("transform", "translate(960,115)")
+				.attr("class","arc");
+	pieD.append("path")
+		.attr("d",arc)
+		.style("fill",function (d, i) {if (i==1) return "#A28BBC"; else return "#D6B6EF";});
+	pieD.append("path")
+		.attr("d",innerarc)
+		.style("fill",function (d, i) {if (i==1) return "#826CA3"; else return "#BBA3D1";});
+	pieD.append("text")
+		.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+		.attr("dy","0.3em")
+		.attr("fill","white").attr("font-size","25px")
+		.style("text-anchor", "middle")
+		.text(function (d, i) {if (i==1) return "80"; else return "20";});
+
+	var rank_text = test_fail_svg.append("g").attr("class","rank_label");
+	rank_text.append("text").attr("transform","translate(150,115)").attr("dy","0.3em").attr("font-size","30px").style("text-anchor", "middle").text("A");
+	rank_text.append("text").attr("transform","translate(420,115)").attr("dy","0.3em").attr("font-size","30px").style("text-anchor", "middle").text("B");
+	rank_text.append("text").attr("transform","translate(690,115)").attr("dy","0.3em").attr("font-size","30px").style("text-anchor", "middle").text("C");
+	rank_text.append("text").attr("transform","translate(960,115)").attr("dy","0.3em").attr("font-size","30px").style("text-anchor", "middle").text("D");
+
+}
+
+function drawDeviceFailure () {
 
 	var data = [
 		{
@@ -153,7 +263,7 @@ function drawTestFailure () {
 		}
 	]
 
-	var test_fail_svg = d3.select("#test_failure").append("svg")
+	var device_fail_svg = d3.select("#device_failure").append("svg")
 								.attr("width",1150).attr("height",400);
 
 	var x_domain = [];
@@ -164,7 +274,7 @@ function drawTestFailure () {
 	var x_scale = d3.scale.ordinal().domain(x_domain).rangeRoundBands([100,1000],0.2,0.2);
 	var y_scale = d3.scale.linear().domain([0,10]).range([350,50]);
 
-	var x_axis = test_fail_svg.append("g")
+	var x_axis = device_fail_svg.append("g")
 				.attr("class", "x axis")
 				.attr("transform", "translate(0,350)")
 				.call(d3.svg.axis()
@@ -172,14 +282,14 @@ function drawTestFailure () {
 					.innerTickSize(3)
 					.outerTickSize(0));
 
-	var y_axis = test_fail_svg.append("g")
+	var y_axis = device_fail_svg.append("g")
 				.attr("class", "y axis")
 				.attr("transform","translate(100,0)")
 				.call(d3.svg.axis()
 					.scale(y_scale)
 					.orient("left"));
 
-	var columns = test_fail_svg.selectAll("g column")
+	var columns = device_fail_svg.selectAll("g column")
 		.data(data).enter().append("g")
 			.attr("class","device_col")
 			.attr("transform",function (d) {return "translate("+x_scale(d.device_name)+",0)"})
@@ -187,12 +297,11 @@ function drawTestFailure () {
 		.data(function (d) {return d.fail_data}).enter();
 	columns.append("rect")
 			.attr("width",50).attr("height",function (d) {return y_scale(10-d.num)-50})
-			.attr("x",0).attr("y",function (d) {return y_scale(d.place)})
+			.attr("x",10).attr("y",function (d) {return y_scale(d.place)})
 			.attr("fill",function (d, i) {if (i==0) return "#EA7C4B"; else if (i==1) return "#ED9FBD"; else if (i==2) return "#52C4D0"; else return "#D6B6EF";});
-	
-	// columns.append("rect")
-	// 		.attr("width", ).attr("height", )
-	// 		.attr("x", ).attr("y", )
-	// 		.attr("fill",function (d, i) {if (i==0) return "#C1633E"; else if (i==1) return "#BF7593"; else if (i==2) return "#34989A"; else return "#A28BBC";});
+	columns.append("rect")
+			.attr("width",10).attr("height",function (d) {return y_scale(10-d.num)-50})
+			.attr("x",60).attr("y",function (d) {return y_scale(d.place)})
+			.attr("fill",function (d, i) {if (i==0) return "#C1633E"; else if (i==1) return "#BF7593"; else if (i==2) return "#34989A"; else return "#A28BBC";});
 
 }
