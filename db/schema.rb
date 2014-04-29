43613e1ea1f9249398244ea1d87112b353d07f00
Kangho Yoon <kangho99@naver.com>
2014-04-29 14:32:31 +0900

    fix: page layout (header-sidebar-content) is now independent from bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -90,14 +90,16 @@ ActiveRecord::Schema.define(version: 20140323032853) do
   add_index "test_scenarios", ["project_id"], name: "index_test_scenarios_on_project_id"
 
   create_table "total_reports", force: true do |t|
-    t.string   "apk"
     t.string   "test_datetime"
+    t.boolean  "status",        default: false
+    t.integer  "apk_id"
     t.integer  "project_id"
     t.integer  "deviceship_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  add_index "total_reports", ["apk_id"], name: "index_total_reports_on_apk_id"
   add_index "total_reports", ["project_id"], name: "index_total_reports_on_project_id"
 
   create_table "users", force: true do |t|
