a863804b24e037f16bd57abbcc3616aef38bd201
flowkater <chjw102@gmail.com>
2014-04-29 20:44:06 +0900

    feat: ScreenShot Image url mount

--- app/models/screen.rb
+++ app/models/screen.rb
@@ -1,2 +1,5 @@
 class Screen < ActiveRecord::Base
+	mount_uploader :url, ScreenshotUploader
+	# belongs_to :device, foreign_key: 'device_key', primary_key: 'device_key'
+	# has_many :detail_reports, foreign_key: 'device_key', primary_key: 'device_key'
 end
