760dbc964d0d3d6b8a8d5e63aefe7bec8a1cd5ce
flowkater <chjw102@gmail.com>
2014-05-14 13:49:17 +0900

    feat: apk model add attributes test_bed_apk

--- db/migrate/20140323032853_create_apks.rb
+++ db/migrate/20140323032853_create_apks.rb
@@ -3,6 +3,7 @@ class CreateApks < ActiveRecord::Migration
     create_table :apks do |t|
       t.string :apk
       t.string :test_apk
+      t.string :test_bed_apk
       t.string :package_name
       t.string :activity_name
       t.string :min_sdk
