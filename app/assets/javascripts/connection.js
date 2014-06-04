0d4ef78a651adc2d8a17567ddbae8b43a1369e13
flowkater <chjw102@gmail.com>
2014-06-04 14:23:54 +0900

    feat: realtime connection test

--- app/assets/javascripts/connection.js
+++ app/assets/javascripts/connection.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-	var socket = io.connect("localhost:5001");
+	var socket = io.connect("http://0.0.0.0:5001");
 	
 	socket.on("rt-change", function(message){
 		console.log(message);
+		$('.device-list').append('<li>'+ message + '</li>')
 	});
 });
